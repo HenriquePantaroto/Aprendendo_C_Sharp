3053cb5da88fd83e6eb5b793632abd9840c18a90
Henrique Pantaroto <76695749+HenriquePantaroto@users.noreply.github.com>
2021-02-25 15:31:26 -0300

    Novo Projeto CSharp

---
+++ P12-CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P12_CalculaInvestimentoLongoPrazo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+            Console.WriteLine("Fim do Programa.");
+        }
+    }
+}
